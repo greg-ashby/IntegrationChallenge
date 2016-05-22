5aabc8bfb995678c0ade77dbafcd743ed0be612f
greg-ashby <ashbygreg@gmail.com>
2016-05-22 11:36:59 -0600

    Testing connection to an integration end point

--- src/main/java/com/gregashby/challenge/MyApp.java
+++ src/main/java/com/gregashby/challenge/MyApp.java
@@ -1,11 +1,17 @@
 package com.gregashby.challenge;
 
+import static spark.Spark.exception;
 import static spark.Spark.get;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import spark.servlet.SparkApplication;
 
 public class MyApp implements SparkApplication {
 
+	private static Logger logger = LoggerFactory.getLogger("default");
+	
 	/**
 	 * This allows you to run as a java app with the embedded Jetty webserver
 	 * 
@@ -14,12 +20,30 @@ public class MyApp implements SparkApplication {
 	public static void main(String[] args) {
 		new MyApp().init();
 	}
-	
+
 	/**
-	 * This allows you to run as a war file deployed on an external server (e.g. Tomcat)
+	 * This allows you to run as a war file deployed on an external server (e.g.
+	 * Tomcat)
 	 */
 	public void init() {
-		get("/hello", (req, res) -> "Hello Greg! Congrats on making your first Java app in many years!");
+	    
+		initSubscriptionApis();
+		
+		get("/*", (request, response) -> "Welcome to my app");
+
+		exception(Exception.class, (exception, request, response) -> {
+			response.body("whoops, something bad happened");
+		});
+
+	}
+
+	private void initSubscriptionApis() {
+		
+		get("/subscription/create", (request, response) -> {
+			logger.info("I was invoked!");
+			return new String("test");
+		});
+		
 	}
 
 }
