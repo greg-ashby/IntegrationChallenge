20f7dacea0b62e965493a3720792d48e6d08759c
greg-ashby <ashbygreg@gmail.com>
2016-05-22 14:53:06 -0600

    added better logging

--- src/main/java/com/gregashby/challenge/MyApp.java
+++ src/main/java/com/gregashby/challenge/MyApp.java
@@ -6,8 +6,7 @@ import static spark.Spark.get;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.Gson;
-
+import spark.Request;
 import spark.servlet.SparkApplication;
 
 public class MyApp implements SparkApplication {
@@ -41,12 +40,28 @@ public class MyApp implements SparkApplication {
 
 	private void initSubscriptionApis() {
 		
-		get("/subscription/create", (request, response) -> {
-			logger.info("----- Request -----");
-			logger.info(request.uri());
-			logger.info(request.url());
+		get("/subscription/create/:event", (request, response) -> {
+			logRequest(request);
 			return new String("test");
 		});
+		
+	}
+	
+	private void logRequest(Request request){
+		logger.info("----- NEW REQUEST -----");
+		logger.info("BODY: {}", request.body());
+		logger.info("ATTRIBUTES: {}", request.attributes().toString());
+		logger.info("CONTENT TYPE: {}", request.contentType());
+		logger.info("CONTEXT PATH: {}", request.contextPath());
+		logger.info("HEADERS: {}", request.headers().toString());
+		logger.info("PARAMS: {}", request.params().toString());
+		logger.info("PROTOCOL: {}", request.protocol());
+		logger.info("QUERY MAP: {}", request.queryMap().toString());
+		logger.info("QUERY PARAMS: {}", request.queryParams().toString());
+		logger.info("REQUEST METHOD: {}", request.requestMethod());
+		logger.info("SCHEME: {}", request.scheme());
+		logger.info("URI: {}", request.uri());
+		logger.info("URL: {}", request.url());
 		
 	}
 
