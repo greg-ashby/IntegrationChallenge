d29bce869d865d7ea29e1859efcf5365a3758d7c
greg-ashby <ashbygreg@gmail.com>
2016-05-26 15:41:37 -0600

    redeploying

--- src/main/java/com/gregashby/challenge/utils/Utils.java
+++ src/main/java/com/gregashby/challenge/utils/Utils.java
@@ -1,8 +1,11 @@
 package com.gregashby.challenge.utils;
 
+import static com.gregashby.challenge.MyApp.logger;
+
 public class Utils {
 
 	public static String extractString(String key, String header) {
+		logger.info("key is {}, header is {}", key, header);
 		String temp = header.substring(header.indexOf(key) + key.length());
 		temp = temp.substring(0, temp.indexOf('"'));
 		return temp;
--- src/test/java/com/gregashby/challenge/utils/UtilsTest.java
+++ src/test/java/com/gregashby/challenge/utils/UtilsTest.java
@@ -18,12 +18,16 @@ public class UtilsTest {
 
 	@Test
 	public void test() {
-		String header = "OAuth oauth_consumer_key=\"ashbyintegrationchallenge-117319\", oauth_nonce=\"5672389859504437538\", oauth_signature=\"16ZTZqmGCgCB0ygjvCIpDEDOm%2BM%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1464295907\", oauth_version=\"1.0\"";
-		String key = "oauth_nonce=\"";
-		String expected = "5672389859504437538";
-		String actual = Utils.extractString(key, header);
-		assertEquals(expected, actual);
-				
+		String[] header = {
+				"OAuth oauth_consumer_key=\"ashbyintegrationchallenge-117319\", oauth_nonce=\"5672389859504437538\", oauth_signature=\"16ZTZqmGCgCB0ygjvCIpDEDOm%2BM%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1464295907\", oauth_version=\"1.0\"",
+				"OAuth oauth_consumer_key=\"ashbyintegrationchallenge-117319\", oauth_nonce=\"7558260176916034971\", oauth_signature=\"VQRiJhu%2F3DjiBWYrwiks6iA1w2c%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1464298554\", oauth_version=\"1.0\"" };
+		String[] key = {"oauth_nonce=\"", "oauth_timestamp=\""};
+		String[] expected = {"5672389859504437538", "1464298554"};
+
+		for (int x = 0; x < expected.length; x++) {
+			String actual = Utils.extractString(key[x], header[x]);
+			assertEquals(expected[x], actual);
+		}
 	}
 
 }
